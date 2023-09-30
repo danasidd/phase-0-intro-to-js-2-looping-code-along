1b26f1f28b1919c6bb52ee8073420612d26f7cff
Dana AlSiddig <danlsiddig@gmail.com>
2023-09-30 23:07:14 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+const birthdayName = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(birthdayName, eventName) {
+    const messages = []; 
+  
+    for (let i = 0; i < birthdayName.length; i++) {
+      const message = `Thank you, ${birthdayName[i]}, for the wonderful ${eventName} gift!`;
+      messages.push(message); 
+    }
+  
+    return messages; 
+  }
+  
+  const birthdayMessages = writeCards(birthdayName, "birthday"); 
+  console.log(birthdayMessages);
+
+function countDown(age) {
+  while (age >= 0) {
+    console.log(age);
+    age--;
+  }
+}
+
+console.log(countDown(10))
